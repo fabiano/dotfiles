987620ada588ea2e5064f0ccea8215dd90492db4
Fabiano França <fabiano.franca@gmail.com>
2018-10-06 16:47:10 -0300

    Updating

--- hyper.js
+++ hyper.js
@@ -3,7 +3,7 @@
     copyOnSelect: true,
     fontSize: 16,
     fontFamily: '"Droid Sans Mono", "Courier New", monospace',
-    shell: 'C:\\WINDOWS\\System32\\WindowsPowerShell\\v1.0\\powershell.exe',
+    shell: "C:\\WINDOWS\\System32\\WindowsPowerShell\\v1.0\\powershell.exe",
     shellArgs: [],
     windowSize: [1200, 700],
   },
