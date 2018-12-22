040388a5a68e961f1562b47bee386ce6f6b248fc
Fabiano França <fabiano.franca@gmail.com>
2018-12-22 18:45:41 -0200

    Fix CTRL+arrow keys

--- hyper.js
+++ hyper.js
@@ -1,11 +1,16 @@
 ﻿module.exports = {
   config: {
     copyOnSelect: true,
-    fontSize: 16,
-    fontFamily: '"Droid Sans Mono", "Courier New", monospace',
+    fontSize: 18,
+    fontFamily: '"Fira Code", "Droid Sans Mono", "Courier New", monospace',
     shell: "C:\\WINDOWS\\System32\\WindowsPowerShell\\v1.0\\powershell.exe",
     shellArgs: [],
-    windowSize: [1200, 700],
+    windowSize: [1500, 900],
+  },
+
+  keymaps: {
+    "editor:movePreviousWord": "",
+    "editor:moveNextWord": "",
   },
 
   plugins: ["hyper-snazzy"],
