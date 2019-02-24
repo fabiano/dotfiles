3fcaca1db31546b3de2ad5b832eb19a64468bf23
Fabiano França <fabiano.franca@gmail.com>
2019-02-24 14:30:17 -0300

    Adding Sublime 3 settings and other updates

--- setup-new-machine.sh
+++ setup-new-machine.sh
@@ -14,18 +14,23 @@
   brew update
   brew upgrade
   brew install bash
-  brew install bash-completion
+  brew install bash-completion@2
   brew install git
+  brew install node
+  brew install rust
   brew install vim
   brew install youtube-dl
   brew install zsh
   brew install zsh-syntax-highlighting
   brew cask install 1password
+  brew cask install android-file-transfer
   brew cask install caffeine
   brew cask install firefox
   brew cask install font-fira-code
   brew cask install google-chrome
+  brew cask install sublime-text
   brew cask install the-unarchiver
+  brew cask install transmission
   brew cask install tuxera-ntfs
   brew cask install visual-studio-code
   brew cleanup
@@ -34,14 +39,25 @@
   rm -rf $DOTFILES_INSTALL_DIR
   git clone git@github.com:fabiano/dotfiles.git $DOTFILES_INSTALL_DIR
 
+  # configure bash
+  rm -f $HOME/.bashrc
+  rm -f $HOME/.bash_profile
+  ln -s $DOTFILES_INSTALL_DIR/bash-bashrc $HOME/.bashrc
+  ln -s $DOTFILES_INSTALL_DIR/bash-bashprofile $HOME/.bash_profile
+
+  if ! grep -q /usr/local/bin/bash /etc/shells; then
+    echo /usr/local/bin/bash | sudo tee -a /etc/shells
+  fi
+
   # configure zsh
-  rm $HOME/.zshrc
+  rm -f $HOME/.zshrc
   ln -s $DOTFILES_INSTALL_DIR/zsh-zshrc $HOME/.zshrc
 
   if ! grep -q /usr/local/bin/zsh /etc/shells; then
     echo /usr/local/bin/zsh | sudo tee -a /etc/shells
   fi
 
+  # set zsh as default shell
   chsh -s /usr/local/bin/zsh
 
   # configure pure
@@ -51,18 +67,29 @@
   ln -s $HOME/.pure/async.zsh $HOME/.pure/async
 
   # configure git
-  rm $HOME/.gitconfig
+  rm -f $HOME/.gitconfig
   ln -s $DOTFILES_INSTALL_DIR/git-gitconfig $HOME/.gitconfig
 
   # configure vim
-  rm $HOME/.vimrc
+  rm -f $HOME/.vimrc
   rm -rf $HOME/.vim
   ln -s $DOTFILES_INSTALL_DIR/vim-vimrc $HOME/.vimrc
-  curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+  curl -fLo $HOME/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
   vim -c 'PlugInstall' -c 'qa!'
 
   # configure visual studio code
-  rm ~/Library/Application\ Support/Code/User/settings.json
+  rm -rf $HOME/Library/Application\ Support/Code
+  mkdir -p $HOME/Library/Application\ Support/Code/User
   ln -s $DOTFILES_INSTALL_DIR/vscode-settings.json $HOME/Library/Application\ Support/Code/User/settings.json
-  IFS=$'\r\n'; for line in `cat vscode-extensions.txt`; do code --install-extension ${line}; done
+  IFS=$'\r\n'; for line in `cat $DOTFILES_INSTALL_DIR/vscode-extensions.txt`; do code --install-extension ${line}; done
+
+  # configure sublime text
+  rm -rf $HOME/Library/Application\ Support/Sublime\ Text\ 3
+  mkdir -p $HOME/Library/Application\ Support/Sublime\ Text\ 3/Packages/User
+  ln -s $DOTFILES_INSTALL_DIR/sublime-preferences.sublime-settings $HOME/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Preferences.sublime-settings
+  ln -s $DOTFILES_INSTALL_DIR/sublime-package-control.sublime-settings $HOME/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Package\ Control.sublime-settings
+  curl -fLo $HOME/Library/Application\ Support/Sublime\ Text\ 3/Installed\ Packages/Package\ Control.sublime-package --create-dirs https://packagecontrol.io/Package%20Control.sublime-package
+
+  # π
+  mkdir -p $HOME/π
 }
