7dd10a1545e2cdb7c2c7e04497fdd2a368bbcaa4
Fabiano França <fabiano.franca@gmail.com>
2019-03-02 17:35:11 -0300

    Fix PowerShell installer filename and show commands output

--- setup-new-machine.ps1
+++ setup-new-machine.ps1
@@ -11,14 +11,14 @@ function New-SymbolicLink ($Path, $Value) {
   $Parent = Split-Path -Path $Path
 
   if (-Not (Test-Path $Parent)) {
-    New-Item -ItemType Directory -Path $Parent | Out-Null
+    New-Item -ItemType Directory -Path $Parent
   }
 
   if (Test-Path $Path) {
-    Remove-Item -Path $Path | Out-Null
+    Remove-Item -Path $Path
   }
 
-  New-Item -ItemType SymbolicLink -Path $Path -Value $Value | Out-Null
+  New-Item -ItemType SymbolicLink -Path $Path -Value $Value
 }
 
 # dotfiles folder
@@ -26,43 +26,43 @@ $DOTFILES_INSTALL_DIR="$HOME\.dotfiles"
 
 # install dependencies
 if (-Not (Test-Path -Path "${env:PROGRAMFILES}\Git\bin\git.exe")) {
-  Invoke-WebRequest -Uri "https://github.com/git-for-windows/git/releases/download/v2.19.0.windows.1/Git-2.19.0-64-bit.exe" -Outfile "f-git.exe" | Out-Null
+  Invoke-WebRequest -Uri "https://github.com/git-for-windows/git/releases/download/v2.19.0.windows.1/Git-2.19.0-64-bit.exe" -Outfile "f-git.exe"
 
-  .\f-git.exe /SP- /VERYSILENT /SUPPRESSMSGBOXES /COMPONENTS="gitlfs,autoupdate" /LOG="f-git.log" | Out-Null
+  .\f-git.exe /SP- /VERYSILENT /SUPPRESSMSGBOXES /COMPONENTS="gitlfs,autoupdate" /LOG="f-git.log"
 
-  Remove-Item -Path f-git.exe | Out-Null
+  Remove-Item -Path f-git.exe
 }
 
 if (-Not (Test-Path -Path "${env:PROGRAMFILES}\PowerShell\6\pwsh.exe")) {
-  Invoke-WebRequest -Uri "https://github.com/PowerShell/PowerShell/releases/download/v6.1.2/PowerShell-6.1.2-win-x64.msi" -Outfile "f-pwsh.msi" | Out-Null
+  Invoke-WebRequest -Uri "https://github.com/PowerShell/PowerShell/releases/download/v6.1.2/PowerShell-6.1.2-win-x64.msi" -Outfile "f-pwsh.msi"
 
-  .\f-pwsh.msi /qn /norestart /l*v "f-pwsh.log" ADD_PATH=1 REGISTER_MANIFEST=1 ENABLE_PSREMOTING=0 ADD_EXPLORER_CONTEXT_MENU_OPENPOWERSHELL=1 | Out-Null
+  .\f-pwsh.msi /qn /norestart /l*v "f-pwsh.log" ADD_PATH=1 REGISTER_MANIFEST=1 ENABLE_PSREMOTING=0 ADD_EXPLORER_CONTEXT_MENU_OPENPOWERSHELL=1
 
-  Remove-Item -Path f-pwsh.exe | Out-Null
+  Remove-Item -Path f-pwsh.msi
 }
 
 if (-Not (Test-Path -Path "${env:LOCALAPPDATA}\hyper\Hyper.exe")) {
-  Invoke-WebRequest -Uri "https://releases.hyper.is/download/win" -Outfile "f-hyper.exe" | Out-Null
+  Invoke-WebRequest -Uri "https://releases.hyper.is/download/win" -Outfile "f-hyper.exe"
 
-  .\f-hyper.exe --silent | Out-Null
+  .\f-hyper.exe --silent
 
-  Remove-Item -Path f-hyper.exe | Out-Null
+  Remove-Item -Path f-hyper.exe
 }
 
 if (-Not (Test-Path -Path "${env:PROGRAMFILES(x86)}\Vim\vim81\vim.exe")) {
-  Invoke-WebRequest -Uri "ftp://ftp.vim.org/pub/vim/pc/gvim81.exe" -Outfile "f-gvim.exe" | Out-Null
+  Invoke-WebRequest -Uri "ftp://ftp.vim.org/pub/vim/pc/gvim81.exe" -Outfile "f-gvim.exe"
 
-  .\f-gvim.exe | Out-Null
+  .\f-gvim.exe
 
-  Remove-Item -Path f-gvim.exe | Out-Null
+  Remove-Item -Path f-gvim.exe
 }
 
 if (-Not (Test-Path -Path "${env:LOCALAPPDATA}\Programs\Microsoft VS Code\Code.exe")) {
-  Invoke-WebRequest -Uri "https://aka.ms/win32-x64-user-stable" -Outfile "f-vscode.exe" | Out-Null
+  Invoke-WebRequest -Uri "https://aka.ms/win32-x64-user-stable" -Outfile "f-vscode.exe"
 
-  .\f-vscode.exe /SP- /VERYSILENT /SUPPRESSMSGBOXES /TASKS="addcontextmenufiles,addcontextmenufolders,addtopath" /LOG="f-vscode.log" | Out-Null
+  .\f-vscode.exe /SP- /VERYSILENT /SUPPRESSMSGBOXES /TASKS="addcontextmenufiles,addcontextmenufolders,addtopath" /LOG="f-vscode.log"
 
-  Remove-Item -Path f-vscode.exe | Out-Null
+  Remove-Item -Path f-vscode.exe
 }
 
 # set environment variables
@@ -85,22 +85,22 @@ New-SymbolicLink -Path $HOME\.gitconfig -Value $DOTFILES_INSTALL_DIR\git-gitconf
 # configure powershell core
 New-SymbolicLink -Path "$HOME\Documents\PowerShell\Profile.ps1" -Value $DOTFILES_INSTALL_DIR\powershell-profile.ps1
 
-PowerShellGet\Install-Module -Name posh-git -Scope CurrentUser -AllowPrerelease -Force | Out-Null
-PowerShellGet\Install-Module -Name PSColor -Scope CurrentUser -AllowPrerelease -Force | Out-Null
+PowerShellGet\Install-Module -Name posh-git -Scope CurrentUser -AllowPrerelease -Force
+PowerShellGet\Install-Module -Name PSColor -Scope CurrentUser -AllowPrerelease -Force
 
 # configure hyper
 New-SymbolicLink -Path $HOME\.hyper.js -Value $DOTFILES_INSTALL_DIR\hyper.js
 
-hyper install hyper-snazzy | Out-Null
+hyper install hyper-snazzy
 
 # configure vim
 New-SymbolicLink -Path $HOME\.vimrc -Value $DOTFILES_INSTALL_DIR\vim-vimrc
 
 if (-Not (Test-Path -Path $HOME/.vim/autoload)) {
-  New-Item -ItemType Directory -Path $HOME/.vim/autoload | Out-Null
+  New-Item -ItemType Directory -Path $HOME/.vim/autoload
 }
 
-Invoke-WebRequest -Uri https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim -Outfile $HOME/.vim/autoload/plug.vim | Out-Null
+Invoke-WebRequest -Uri https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim -Outfile $HOME/.vim/autoload/plug.vim
 
 vim -c "PlugInstall" -c "qa!"
 
@@ -110,12 +110,12 @@ New-SymbolicLink -Path $HOME\AppData\Roaming\Code\User\settings.json -Value $DOT
 Get-Content -Path $DOTFILES_INSTALL_DIR\vscode-extensions.txt | ForEach-Object { code --install-extension $_ }
 
 # copy fonts
-Invoke-WebRequest -Uri "https://github.com/aosp-mirror/platform_frameworks_base/raw/master/data/fonts/DroidSansMono.ttf" -Outfile font-droid-sans-mono.ttf | Out-Null
-Invoke-WebRequest -Uri "https://github.com/tonsky/FiraCode/raw/master/distr/ttf/FiraCode-Regular.ttf" -Outfile font-firacode-regular.ttf | Out-Null
+Invoke-WebRequest -Uri "https://github.com/aosp-mirror/platform_frameworks_base/raw/master/data/fonts/DroidSansMono.ttf" -Outfile font-droid-sans-mono.ttf
+Invoke-WebRequest -Uri "https://github.com/tonsky/FiraCode/raw/master/distr/ttf/FiraCode-Regular.ttf" -Outfile font-firacode-regular.ttf
 
 $SA = New-Object -ComObject Shell.Application
 $Fonts = $SA.NameSpace(0x14)
 
 Get-ChildItem -Path font-*.ttf | ForEach-Object { $Fonts.CopyHere($_.FullName) }
 
-Remove-Item -Path font-*.ttf -Recurse | Out-Null
+Remove-Item -Path font-*.ttf -Recurse
