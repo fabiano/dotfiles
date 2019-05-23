258d89eba1e4ec10b2dc0197477dc3e3a731cded
Fabiano França <fabiano.franca@gmail.com>
2019-05-23 20:24:10 -0300

    Update powershell-profile.ps1

--- powershell-profile.ps1
+++ powershell-profile.ps1
@@ -12,7 +12,56 @@ function X ($Name) {
 
 function Prompt {
   Write-Host ""
-  Write-Host $PWD -ForegroundColor DarkBlue
+  Write-Host $PWD -NoNewLine -ForegroundColor DarkBlue
+
+  $status = git status --short --branch
+
+  if ($?) {
+    $changes = @{
+      untracked = 0
+      added     = 0
+      modified  = 0
+      deleted   = 0
+      unmerged  = 0
+    }
+
+    switch -regex ($status) {
+      "^## (No commits yet on|Initial commit on) (?<branchName>\S+)$" {
+        $branchName = $matches["branchName"]
+
+        continue
+      }
+
+      "^## (?<branchName>\S+?)(\.\.\.(?<upstream>\S+))?$" {
+        $branchName = $matches["branchName"]
+
+        continue
+      }
+
+      "^(?<index>.*) (?<filename>.*)$" {
+        switch ($matches["index"].Trim()) {
+          "??" { $changes.untracked += 1; break }
+          "A"  { $changes.added     += 1; break }
+          "M"  { $changes.modified  += 1; break }
+          "R"  { $changes.modified  += 1; break }
+          "C"  { $changes.modified  += 1; break }
+          "D"  { $changes.deleted   += 1; break }
+          "U"  { $changes.unmerged  += 1; break }
+        }
+
+        continue
+      }
+    }
+
+    Write-Host -NoNewLine " $branchName" -ForegroundColor DarkGray
+    Write-Host -NoNewLine " ?$($changes.untracked)" -ForegroundColor DarkGray
+    Write-Host -NoNewLine " +$($changes.added)" -ForegroundColor DarkGray
+    Write-Host -NoNewLine " ~$($changes.modified)" -ForegroundColor DarkGray
+    Write-Host -NoNewLine " -$($changes.deleted)" -ForegroundColor DarkGray
+    Write-Host -NoNewLine " !$($changes.unmerged)" -ForegroundColor DarkGray
+  }
+
+  Write-Host ""
 
   if ($env:HYPER -Eq "1") {
     Write-Host "❯❯" -NoNewLine -ForegroundColor DarkMagenta
