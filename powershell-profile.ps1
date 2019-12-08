cf836ba95304de123eb1c5548e6d4d4c2670d6f4
Fabiano França <fabiano.franca@gmail.com>
2019-12-08 10:12:44 -0300

    Update powershell-profile.ps1

--- powershell-profile.ps1
+++ powershell-profile.ps1
@@ -14,20 +14,20 @@ function Prompt {
   Write-Host ""
   Write-Host $PWD -NoNewLine -ForegroundColor DarkBlue
 
-  $status = git status --short --branch
+  $Status = git status --short --branch
 
   if ($?) {
-    $changes = @{
-      untracked = 0
-      added     = 0
-      modified  = 0
-      deleted   = 0
-      unmerged  = 0
+    $Changes = @{
+      Untracked = 0
+      Added     = 0
+      Modified  = 0
+      Deleted   = 0
+      Unmerged  = 0
     }
 
-    switch -regex ($status) {
+    switch -regex ($Status) {
       "^## (No commits yet on|Initial commit on) (?<branchName>\S+)$" {
-        $branchName = $matches["branchName"]
+        $BranchName = $matches["branchName"]
 
         continue
       }
@@ -45,45 +45,45 @@ function Prompt {
         ## feature/ping...origin/feature/ping [ahead 1, behind 1]
       #>
       "^##\s(?<branchName>\S+?)(\.{3}(?<upstream>\S+?))?(\s\[(ahead\s(?<ahead>\d*))?((,\s)?behind\s(?<behind>\d*))?\])?$" {
-        $branchName = $matches["branchName"]
-        $ahead = $matches["ahead"]
-        $behind = $matches["behind"]
+        $BranchName = $matches["branchName"]
+        $Ahead = $matches["ahead"]
+        $Behind = $matches["behind"]
 
         continue
       }
 
       "^(?<index>.*) (?<filename>.*)$" {
         switch ($matches["index"].Trim()) {
-          "??" { $changes.untracked += 1; break }
-          "A"  { $changes.added     += 1; break }
-          "M"  { $changes.modified  += 1; break }
-          "R"  { $changes.modified  += 1; break }
-          "C"  { $changes.modified  += 1; break }
-          "D"  { $changes.deleted   += 1; break }
-          "U"  { $changes.unmerged  += 1; break }
+          "??" { $Changes.Untracked += 1; break }
+          "A"  { $Changes.Added     += 1; break }
+          "M"  { $Changes.Modified  += 1; break }
+          "R"  { $Changes.Modified  += 1; break }
+          "C"  { $Changes.Modified  += 1; break }
+          "D"  { $Changes.Deleted   += 1; break }
+          "U"  { $Changes.Unmerged  += 1; break }
         }
 
         continue
       }
     }
 
-    Write-Host -NoNewLine " $branchName" -ForegroundColor DarkGray
+    Write-Host -NoNewLine " $BranchName" -ForegroundColor DarkGray
 
-    if ($ahead -And $behind) {
-      Write-Host -NoNewLine " $ahead↕$behind" -ForegroundColor DarkGray
-    } elseif ($ahead) {
-      Write-Host -NoNewLine " ↑$ahead" -ForegroundColor DarkGray
-    } elseif ($behind) {
-      Write-Host -NoNewLine " ↓$behind" -ForegroundColor DarkGray
+    if ($Ahead -And $Behind) {
+      Write-Host -NoNewLine " $Ahead↕$Behind" -ForegroundColor DarkGray
+    } elseif ($Ahead) {
+      Write-Host -NoNewLine " ↑$Ahead" -ForegroundColor DarkGray
+    } elseif ($Behind) {
+      Write-Host -NoNewLine " ↓$Behind" -ForegroundColor DarkGray
     } else {
       Write-Host -NoNewLine " ≡" -ForegroundColor DarkGray
     }
 
-    Write-Host -NoNewLine " ?$($changes.untracked)" -ForegroundColor DarkGray
-    Write-Host -NoNewLine " +$($changes.added)" -ForegroundColor DarkGray
-    Write-Host -NoNewLine " ~$($changes.modified)" -ForegroundColor DarkGray
-    Write-Host -NoNewLine " -$($changes.deleted)" -ForegroundColor DarkGray
-    Write-Host -NoNewLine " !$($changes.unmerged)" -ForegroundColor DarkGray
+    Write-Host -NoNewLine " ?$($Changes.Untracked)" -ForegroundColor DarkGray
+    Write-Host -NoNewLine " +$($Changes.Added)" -ForegroundColor DarkGray
+    Write-Host -NoNewLine " ~$($Changes.Modified)" -ForegroundColor DarkGray
+    Write-Host -NoNewLine " -$($Changes.Deleted)" -ForegroundColor DarkGray
+    Write-Host -NoNewLine " !$($Changes.Unmerged)" -ForegroundColor DarkGray
   }
 
   Write-Host ""
@@ -107,7 +107,7 @@ function MoveUp {
 }
 
 function Start-HttpServer ($Port) {
-  & python -m http.server $Port
+  & npx http-server . --port $Port
 }
 
 # alias
