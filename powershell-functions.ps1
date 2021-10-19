c2354e0035675e93e405d6ad9604022963859578
Fabiano França <fabiano.franca@gmail.com>
2021-10-19 08:38:39 -0300

    Update PowerShell version

--- powershell-functions.ps1
+++ powershell-functions.ps1
@@ -207,7 +207,7 @@ function Install-Node {
 # install powershell
 function Install-PowerShell {
   Install-App `
-    -URL "https://github.com/PowerShell/PowerShell/releases/download/v7.1.4/PowerShell-7.1.4-win-x64.msi" `
+    -URL "https://github.com/PowerShell/PowerShell/releases/download/v7.1.5/PowerShell-7.1.5-win-x64.msi" `
     -OutFile "setup-powershell.msi" `
     -Arguments @(
       "/passive"
