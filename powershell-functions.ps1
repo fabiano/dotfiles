42ed4c4a2fa05d70d83c1e927fc3c718593dbb20
Fabiano França <fabiano.franca@gmail.com>
2022-01-08 21:59:49 -0300

    Update powershell-functions.ps1

--- powershell-functions.ps1
+++ powershell-functions.ps1
@@ -301,7 +301,7 @@ function Install-Typora {
 # install visual studio community
 function Install-VSCommunity {
   Install-App `
-    -URL "https://download.visualstudio.microsoft.com/download/pr/9b3476ff-6d0a-4ff8-956d-270147f21cd4/76e39c746d9e2fc3eadd003b5b11440bcf926f3948fb2df14d5938a1a8b2b32f/vs_Community.exe" `
+    -URL "https://aka.ms/vs/17/release/vs_community.exe" `
     -OutFile "setup-vs-community.exe" `
     -Arguments @()
 }
