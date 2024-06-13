2ec232a9f95b3952e7c882d0d0078911a2f07a1b
Fabiano França <fabiano.franca@gmail.com>
2024-06-13 18:12:38 +0100

    Update mac os setup script

--- setup-new-macos.sh
+++ setup-new-macos.sh
@@ -10,7 +10,6 @@
   fi
 
   # install dependencies
-  brew tap homebrew/cask-drivers
   brew tap homebrew/cask-fonts
   brew tap homebrew/cask-versions
   brew update
@@ -18,18 +17,15 @@
   brew install bash
   brew install bash-completion@2
   brew install git
+  brew install helix
+  brew install starship
   brew install vim
   brew install zsh
   brew install zsh-autosuggestions
   brew install zsh-syntax-highlighting
-  brew install --cask caffeine
   brew install --cask firefox
-  brew install --cask font-fira-code
   brew install --cask font-iosevka
   brew install --cask forklift
-  brew install --cask google-chrome
-  brew install --cask iterm2
-  brew install --cask microsoft-edge
   brew install --cask the-unarchiver
   brew install --cask visual-studio-code
   brew cleanup
@@ -38,19 +34,10 @@
   rm -rf $DOTFILES_INSTALL_DIR
   git clone $DOTFILES_REPOSITORY $DOTFILES_INSTALL_DIR
 
-  # configure bash
-  rm -f $HOME/.bashrc
-  rm -f $HOME/.bash_profile
-  ln -s $DOTFILES_INSTALL_DIR/bash-bashrc $HOME/.bashrc
-  ln -s $DOTFILES_INSTALL_DIR/bash-bashprofile $HOME/.bash_profile
-
+  # add bash and zsh to shells file
   if ! grep -q /usr/local/bin/bash /etc/shells; then
     echo /usr/local/bin/bash | sudo tee -a /etc/shells
   fi
-
-  # configure zsh
-  rm -f $HOME/.zshrc
-  ln -s $DOTFILES_INSTALL_DIR/zsh-zshrc $HOME/.zshrc
 
   if ! grep -q /usr/local/bin/zsh /etc/shells; then
     echo /usr/local/bin/zsh | sudo tee -a /etc/shells
@@ -59,26 +46,33 @@
   # set zsh as default shell
   chsh -s /usr/local/bin/zsh
 
-  # configure pure
-  rm -rf $HOME/.pure
-  git clone https://github.com/sindresorhus/pure.git $HOME/.pure
-  ln -s $HOME/.pure/pure.zsh $HOME/.pure/prompt_pure_setup
-  ln -s $HOME/.pure/async.zsh $HOME/.pure/async
-
-  # configure git
-  rm -f $HOME/.gitconfig
-  ln -s $DOTFILES_INSTALL_DIR/git-gitconfig $HOME/.gitconfig
-
-  # configure vim
-  rm -f $HOME/.vimrc
-  rm -rf $HOME/.vim
-  ln -s $DOTFILES_INSTALL_DIR/vim-vimrc $HOME/.vimrc
-  curl -fLo $HOME/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-  vim -c 'PlugInstall' -c 'qa!'
-
-  # configure visual studio code
+  # create dotfiles
+  rm -rf $HOME/.bash_profile
+  rm -rf $HOME/.bashrc
+  rm -rf $HOME/.config/helix
+  rm -rf $HOME/.config/starship.toml
+  rm -rf $HOME/.gitconfig
+  rm -rf $HOME/.vimrc
+  rm -rf $HOME/.zshrc
   rm -rf $HOME/Library/Application\ Support/Code
+
+  mkdir -p $HOME/.config/helix
+  mkdir -p $HOME/.config/kitty
   mkdir -p $HOME/Library/Application\ Support/Code/User
+
+  ln -s $DOTFILES_INSTALL_DIR/bash-bashprofile $HOME/.bash_profile
+  ln -s $DOTFILES_INSTALL_DIR/bash-bashrc $HOME/.bashrc
+  ln -s $DOTFILES_INSTALL_DIR/git-gitconfig $HOME/.gitconfig
+  ln -s $DOTFILES_INSTALL_DIR/helix-config.toml $HOME/.config/helix/config.toml
+  ln -s $DOTFILES_INSTALL_DIR/starship.toml $HOME/.config/starship.toml
+  ln -s $DOTFILES_INSTALL_DIR/vim-vimrc $HOME/.vimrc
   ln -s $DOTFILES_INSTALL_DIR/vscode-settings.json $HOME/Library/Application\ Support/Code/User/settings.json
+  ln -s $DOTFILES_INSTALL_DIR/zsh-zshrc $HOME/.zshrc
+
+  # install plug
+  rm -rf $HOME/.vim
+  curl -fLo $HOME/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+  # install visual studio code extensions
   IFS=$'\r\n'; for line in `cat $DOTFILES_INSTALL_DIR/vscode-extensions.txt`; do code --install-extension ${line}; done
 }
