94c6c56ef66598f193149e5fd5c93f7516249099
Fabiano França <fabiano.franca@gmail.com>
2024-09-26 09:01:37 +0100

    Add Termux setup script

---
+++ setup-new-termux.sh
@@ -0,0 +1,54 @@
+{
+  # dotfiles settings
+  DOTFILES_REPOSITORY="https://github.com/fabiano/dotfiles.git"
+  DOTFILES_INSTALL_DIR="$HOME/.dotfiles"
+
+  # install apps
+  pkg update
+  pkg upgrade
+  pkg install -y bash-completion
+  pkg install -y bat
+  pkg install -y curl
+  pkg install -y eza
+  pkg install -y git
+  pkg install -y gh
+  pkg install -y helix
+  pkg install -y openssh
+  pkg install -y starship
+  pkg install -y vim
+  pkg install -y zsh
+  pkg autoclean -y
+
+  # github authentication
+  gh auth login
+
+  # clone repository
+  rm -rf $DOTFILES_INSTALL_DIR
+  git clone $DOTFILES_REPOSITORY $DOTFILES_INSTALL_DIR
+
+  # create dotfiles
+  rm -rf $HOME/.bash_profile
+  rm -rf $HOME/.bashrc
+  rm -rf $HOME/.config/helix
+  rm -rf $HOME/.config/starship.toml
+  rm -rf $HOME/.gitconfig
+  rm -rf $HOME/.vimrc
+  rm -rf $HOME/.zshrc
+
+  mkdir -p $HOME/.config/helix
+
+  ln -s $DOTFILES_INSTALL_DIR/bash-bashprofile $HOME/.bash_profile
+  ln -s $DOTFILES_INSTALL_DIR/bash-bashrc $HOME/.bashrc
+  ln -s $DOTFILES_INSTALL_DIR/git-gitconfig $HOME/.gitconfig
+  ln -s $DOTFILES_INSTALL_DIR/helix-config.toml $HOME/.config/helix/config.toml
+  ln -s $DOTFILES_INSTALL_DIR/starship.toml $HOME/.config/starship.toml
+  ln -s $DOTFILES_INSTALL_DIR/vim-vimrc $HOME/.vimrc
+  ln -s $DOTFILES_INSTALL_DIR/zsh-zshrc $HOME/.zshrc
+
+  # set zsh as default shell
+  chsh -s zsh
+
+  # install plug
+  rm -rf $HOME/.vim
+  curl -fLo $HOME/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+}
